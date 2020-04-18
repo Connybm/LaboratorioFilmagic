c1aa4c9fe957b4c8f006047334177f0808b972f6
Conny <cbarriosm1@miumg.edu.gt>
2020-04-17 20:54:05 -0600

    Proceso Renta

---
+++ FilmMagic/Laborartorio_FilmMagic/Consultas/Frm_consultaproducto.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Laborartorio_FilmMagic.Consulta
+{
+    partial class Frm_consultaproducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_consultaproducto));
+            this.Btn_actualizar = new System.Windows.Forms.Button();
+            this.Btn_seleccionar = new System.Windows.Forms.Button();
+            this.Dgv_consultaproveedor = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Pnl_nombreForm = new System.Windows.Forms.Panel();
+            this.btn_minimizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_consultaproveedor)).BeginInit();
+            this.Pnl_nombreForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_actualizar
+            // 
+            this.Btn_actualizar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Btn_actualizar.FlatAppearance.BorderSize = 3;
+            this.Btn_actualizar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_actualizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaGreen;
+            this.Btn_actualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_actualizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_actualizar.Location = new System.Drawing.Point(675, 115);
+            this.Btn_actualizar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_actualizar.Name = "Btn_actualizar";
+            this.Btn_actualizar.Size = new System.Drawing.Size(123, 33);
+            this.Btn_actualizar.TabIndex = 80;
+            this.Btn_actualizar.Text = "Actualizar";
+            this.Btn_actualizar.UseVisualStyleBackColor = true;
+            this.Btn_actualizar.Click += new System.EventHandler(this.Btn_actualizar_Click);
+            // 
+            // Btn_seleccionar
+            // 
+            this.Btn_seleccionar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Btn_seleccionar.FlatAppearance.BorderSize = 3;
+            this.Btn_seleccionar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.Btn_seleccionar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SeaGreen;
+            this.Btn_seleccionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_seleccionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_seleccionar.Location = new System.Drawing.Point(675, 193);
+            this.Btn_seleccionar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Btn_seleccionar.Name = "Btn_seleccionar";
+            this.Btn_seleccionar.Size = new System.Drawing.Size(123, 33);
+            this.Btn_seleccionar.TabIndex = 79;
+            this.Btn_seleccionar.Text = "Seleccionar";
+            this.Btn_seleccionar.UseVisualStyleBackColor = true;
+            this.Btn_seleccionar.Click += new System.EventHandler(this.Btn_seleccionar_Click);
+            // 
+            // Dgv_consultaproveedor
+            // 
+            this.Dgv_consultaproveedor.AllowUserToAddRows = false;
+            this.Dgv_consultaproveedor.AllowUserToDeleteRows = false;
+            this.Dgv_consultaproveedor.AllowUserToResizeColumns = false;
+            this.Dgv_consultaproveedor.AllowUserToResizeRows = false;
+            this.Dgv_consultaproveedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_consultaproveedor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8});
+            this.Dgv_consultaproveedor.Location = new System.Drawing.Point(20, 72);
+            this.Dgv_consultaproveedor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Dgv_consultaproveedor.Name = "Dgv_consultaproveedor";
+            this.Dgv_consultaproveedor.RowHeadersVisible = false;
+            this.Dgv_consultaproveedor.RowHeadersWidth = 51;
+            this.Dgv_consultaproveedor.RowTemplate.Height = 24;
+            this.Dgv_consultaproveedor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Dgv_consultaproveedor.Size = new System.Drawing.Size(631, 241);
+            this.Dgv_consultaproveedor.TabIndex = 78;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "codigo";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Precio";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Categoria";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "estado";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Tipo prodcuto";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 125;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Autor";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 125;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Proveedor";
+            this.Column8.MinimumWidth = 6;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 125;
+            // 
+            // Pnl_nombreForm
+            // 
+            this.Pnl_nombreForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(43)))), ((int)(((byte)(119)))));
+            this.Pnl_nombreForm.Controls.Add(this.btn_minimizar);
+            this.Pnl_nombreForm.Controls.Add(this.label1);
+            this.Pnl_nombreForm.Controls.Add(this.btn_cerrar);
+            this.Pnl_nombreForm.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_nombreForm.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Pnl_nombreForm.Name = "Pnl_nombreForm";
+            this.Pnl_nombreForm.Size = new System.Drawing.Size(830, 51);
+            this.Pnl_nombreForm.TabIndex = 77;
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_minimizar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("btn_minimizar.Image")));
+            this.btn_minimizar.Location = new System.Drawing.Point(722, 11);
+            this.btn_minimizar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(33, 31);
+            this.btn_minimizar.TabIndex = 5;
+            this.btn_minimizar.UseVisualStyleBackColor = true;
+            this.btn_minimizar.Click += new System.EventHandler(this.Btn_minimizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(20, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consulta de Producto";
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_cerrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
+            this.btn_cerrar.Location = new System.Drawing.Point(776, 11);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(33, 31);
+            this.btn_cerrar.TabIndex = 4;
+            this.btn_cerrar.UseVisualStyleBackColor = true;
+            this.btn_cerrar.Click += new System.EventHandler(this.Btn_cerrar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(116)))), ((int)(((byte)(43)))), ((int)(((byte)(119)))));
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(470, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(339, 19);
+            this.label2.TabIndex = 92;
+            this.label2.Text = "Conny María Barrios Moreno - 0901 15 4555";
+            // 
+            // Frm_consultaproducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 374);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Btn_actualizar);
+            this.Controls.Add(this.Btn_seleccionar);
+            this.Controls.Add(this.Dgv_consultaproveedor);
+            this.Controls.Add(this.Pnl_nombreForm);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Frm_consultaproducto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_consultaproducto";
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_consultaproveedor)).EndInit();
+            this.Pnl_nombreForm.ResumeLayout(false);
+            this.Pnl_nombreForm.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_actualizar;
+        private System.Windows.Forms.Button Btn_seleccionar;
+        public System.Windows.Forms.DataGridView Dgv_consultaproveedor;
+        private System.Windows.Forms.Panel Pnl_nombreForm;
+        private System.Windows.Forms.Button btn_minimizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.Label label2;
+    }
+}
